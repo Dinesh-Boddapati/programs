c912cc692aa8ac1fe0cac28e6251e2bb87d0dfc7
Dinesh B <130901291+Dinesh-Boddapati@users.noreply.github.com>
2025-08-15 18:51:18 -0500

    Delete getting-started-todo-app-main directory

--- getting-started-todo-app-main/backend/src/routes/getGreeting.js
+++
@@ -1,7 +0,0 @@
-const GREETING = ['Hello', 'Hi', 'Greetings', 'Welcome', 'Namaste', 'Hola', 'Bonjour', 'Ciao', 'Salam', 'Shalom'];
-
-module.exports = async (req, res) => {
-    res.send({
-        greeting: GREETING[ Math.floor( Math.random() * GREETING.length )],
-    });
-};
